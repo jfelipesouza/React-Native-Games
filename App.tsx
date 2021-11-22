1efe22b012b7a4209868c412a7e88b15b6719e6d
Felipe Souza <j.felipe.dev.18@gmail.com>
2021-11-22 14:09:37 -0300

    Adicionando as fontes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { StatusBar } from "react-native";
-import SingIN from "./src/screens/singIn";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
 import { ThemeProvider } from "styled-components";
 import { light } from "./src/styles/theme";
+import SingIN from "./src/screens/singIn";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={light}>
       <StatusBar backgroundColor={"#0D133E"} barStyle={"light-content"} />
